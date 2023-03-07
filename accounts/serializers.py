a5c365176f710287a1b5f95185cddcedcf840972
Victor Calimano <victorcalimano@gmail.com>
2023-03-06 19:49:43 -0500

    name fields added to api

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -9,15 +9,18 @@ from allauth.account.adapter import get_adapter
 class CustomRegisterSerializer(RegisterSerializer):
     username=None
     first_name = serializers.CharField(required=True, write_only=True)
+    last_name = serializers.CharField(required=True, write_only=True)
     class Meta:
         model = CustomUser
-        fields = ('email', 'password1', 'password2',)
+        fields = ('email', 'password1', 'password2', 'first_name', 'last_name')
     
     def get_cleaned_data(self):
         return {
             'password1': self.validated_data.get('password1', ''),
             'password2': self.validated_data.get('password2', ''),
             'email': self.validated_data.get('email', ''),
+            'first_name': self.validated_data.get('first_name', ''),
+            'Last_name': self.validated_data.get('last_name', ''),
             
         }
 
@@ -26,7 +29,8 @@ class CustomRegisterSerializer(RegisterSerializer):
         adapter = get_adapter()
         user = adapter.new_user(request)
         self.cleaned_data = self.get_cleaned_data()
-        
+        user.first_name = self.validated_data.get('first_name', '')
+        user.last_name = self.validated_data.get('last_name', '')
         user.save()
         adapter.save_user(request, user, self)
         return user
